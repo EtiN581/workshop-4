bfcfb90ecd17c34800b4508c64bcfd3da1d272bb
etienne goury <etienne.goury@edu.devinci.fr>
2024-03-15 19:57:16 +0100

    update

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -14,12 +14,11 @@ export type GetNodeRegistryBody = {
   nodes: Node[];
 };
 
-const registeredNodes: Node[] = [];
-
 export async function launchRegistry() {
   const _registry = express();
   _registry.use(express.json());
   _registry.use(bodyParser.json());
+  let registeredNodes: GetNodeRegistryBody = { nodes: [] }
 
   // TODO implement the status route
   _registry.get("/status", (req, res) => {
@@ -27,13 +26,15 @@ export async function launchRegistry() {
   });
 
   _registry.post("/registerNode", (req, res) => {
-    const { nodeId, pubKey} = req.body as RegisterNodeBody
+    const { nodeId, pubKey} = req.body
     if (!nodeId || !pubKey) {
       res.status(400).json({ error: "Node ID and Public Key are required." })
+    } else if (registeredNodes.nodes.some(n => n.nodeId === nodeId || n.pubKey === pubKey)) {
+      res.send("Node ID or public key already in use")
     } else {
       const newNode: Node = {nodeId, pubKey};
-      registeredNodes.push(newNode);
-      res.status(200).json({ message: "Node registered successfully." })
+      registeredNodes.nodes.push(newNode);
+      res.status(200).json({ result: "Node registered successfully." })
     }
   })
 
@@ -44,53 +45,10 @@ export async function launchRegistry() {
   })
 
   _registry.get("/getNodeRegistry", (req, res) => {
-    const nodesJSON:any = []
-    registeredNodes.forEach(node => {
-      nodesJSON.push({
-        nodeId: node.nodeId,
-        pubKey: node.pubKey
-      })
-    })
-    res.send({ nodes: nodesJSON})
+    res.status(200).json(registeredNodes)
   })
 
-  /*
-  // Generates a pair of private / public RSA keys
-  generateRsaKeyPair(): Promise<GenerateRsaKeyPair>
-
-  // Export a crypto public key to a base64 string format
-  exportPubKey(key: webcrypto.CryptoKey): Promise<string>
-
-  // Export a crypto private key to a base64 string format
-  exportPrvKey(key: webcrypto.CryptoKey | null): Promise<string | null>
-
-  // Import a base64 string public key to its native format
-  importPubKey(strKey: string): Promise<webcrypto.CryptoKey>
-
-  // Import a base64 string private key to its native format
-  importPrvKey(strKey: string): Promise<webcrypto.CryptoKey>
-
-  // Encrypt a message using an RSA public key
-  rsaEncrypt(b64Data: string, strPublicKey: string): Promise<string>
-
-  // Decrypts a message using an RSA private key
-  rsaDecrypt(data: string, privateKey: webcrypto.CryptoKey): Promise<string>
-
-  // Generates a random symmetric key
-  createRandomSymmetricKey(): Promise<webcrypto.CryptoKey>
-
-  // Export a crypto symmetric key to a base64 string format
-  exportSymKey(key: webcrypto.CryptoKey): Promise<string>
-
-  // Import a base64 string format to its crypto native format
-  importSymKey(strKey: string): Promise<webcrypto.CryptoKey>
-
-  // Encrypt a message using a symmetric key
-  symEncrypt(key: webcrypto.CryptoKey, data: string): Promise<string>
-
-  // Decrypt a message using a symmetric key
-  symDecrypt(strKey: string, encryptedData: string): Promise<string>
-  */
+  
 
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
--- src/users/user.ts
+++ src/users/user.ts
@@ -1,14 +1,17 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_USER_PORT } from "../config";
+import { BASE_USER_PORT, REGISTRY_PORT, BASE_ONION_ROUTER_PORT } from "../config";
+import { Node, GetNodeRegistryBody } from "../registry/registry";
+import * as crypto from "../crypto"
 
 export type SendMessageBody = {
   message: string;
   destinationUserId: number;
 };
 
-let lastReceivedMessage:any = null
-let lastSentMessage:any = null
+let lastReceivedMessage:string | null = null
+let lastSentMessage:string | null = null
+let lastCircuit: Node[] = []
 
 export async function user(userId: number) {
   const _user = express();
@@ -21,15 +24,11 @@ export async function user(userId: number) {
   });
 
   _user.get("/getLastReceivedMessage", (req, res)=>{
-    res.send({
-      result:lastReceivedMessage
-    })
+    res.status(200).json({result: lastReceivedMessage})
   })
 
   _user.get("/getLastSentMessage", (req, res)=>{
-    res.send({
-      result:lastSentMessage
-    })
+    res.status(200).json({result:lastSentMessage})
   })
 
   _user.post("/message", (req, res) => {
@@ -37,6 +36,83 @@ export async function user(userId: number) {
     lastReceivedMessage = message
     res.status(200)
   })
+
+  _user.post("/sendMessage", async (req, res) => {
+    const {message, destinationUserId} = req.body
+    let circuit: Node[] = []
+    const nodes = await fetch(`http://localhost:${REGISTRY_PORT}/getNodeRegistry`)
+      .then(res => res.json() as Promise<GetNodeRegistryBody>)
+      .then(body => body.nodes)
+    while (circuit.length<3) {
+      const randomNode = nodes[Math.floor(Math.random()*nodes.length)]
+      if (!circuit.includes(randomNode)) circuit.push(randomNode)
+    }
+    lastSentMessage = message
+    let destination = `${REGISTRY_PORT+destinationUserId}`.padStart(10, "0")
+    let messageToSend = message
+    for (const node of circuit) {
+      const symKey = await crypto.createRandomSymmetricKey()
+      const messageToEncrypt = `${destination + messageToSend}`;
+      destination = `${BASE_ONION_ROUTER_PORT + node.nodeId}`.padStart(10, "0");
+      const encMessage = await crypto.symEncrypt(symKey, `${destination + lastSentMessage}`)
+      const strSymKey = await await crypto.exportSymKey(symKey)
+      const encSymKey = await crypto.rsaEncrypt(strSymKey, node.pubKey)
+      messageToSend = encSymKey + encMessage
+    }
+
+    circuit.reverse()
+    lastCircuit = circuit
+    await fetch(`http://localhost:${BASE_ONION_ROUTER_PORT+circuit[0].nodeId}/message`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: messageToSend
+      })
+    })
+
+    res.status(200).send("Message sent")
+  })
+
+  /*
+  // Generates a pair of private / public RSA keys
+  generateRsaKeyPair(): Promise<GenerateRsaKeyPair>
+
+  // Export a crypto public key to a base64 string format
+  exportPubKey(key: webcrypto.CryptoKey): Promise<string>
+
+  // Export a crypto private key to a base64 string format
+  exportPrvKey(key: webcrypto.CryptoKey | null): Promise<string | null>
+
+  // Import a base64 string public key to its native format
+  importPubKey(strKey: string): Promise<webcrypto.CryptoKey>
+
+  // Import a base64 string private key to its native format
+  importPrvKey(strKey: string): Promise<webcrypto.CryptoKey>
+
+  // Encrypt a message using an RSA public key
+  rsaEncrypt(b64Data: string, strPublicKey: string): Promise<string>
+
+  // Decrypts a message using an RSA private key
+  rsaDecrypt(data: string, privateKey: webcrypto.CryptoKey): Promise<string>
+
+  // Generates a random symmetric key
+  createRandomSymmetricKey(): Promise<webcrypto.CryptoKey>
+
+  // Export a crypto symmetric key to a base64 string format
+  exportSymKey(key: webcrypto.CryptoKey): Promise<string>
+
+  // Import a base64 string format to its crypto native format
+  importSymKey(strKey: string): Promise<webcrypto.CryptoKey>
+
+  // Encrypt a message using a symmetric key
+  symEncrypt(key: webcrypto.CryptoKey, data: string): Promise<string>
+
+  // Decrypt a message using a symmetric key
+  symDecrypt(strKey: string, encryptedData: string): Promise<string>
+  */
+
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
